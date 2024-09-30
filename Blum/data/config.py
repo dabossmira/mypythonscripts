938bebad1d8ebd93b49f1fac4b217ef2c29e11d5
Onoriode Avwenagbiku <91702198+dabossmira@users.noreply.github.com>
2024-09-30 22:50:35 +0100

    commit

---
+++ Blum/data/config.py
@@ -0,0 +1,39 @@
+API_ID = 20597659
+API_HASH =  "ec1feb0a90611c5ab9c8b96685c288f8"
+# delay between connections to accounts
+ACC_DELAY = [5, 15]
+
+# folder with sessions (do not change)
+WORKDIR = "sessions/"
+
+# Using proxy
+USE_PROXY = False # True/False
+
+
+PROXY_TYPE = "socks5"
+
+# Full link to your Sessions folder
+SESSIONS_PATH = "YOUR PATH TO SESSIONS"
+
+# How many points from the game
+POINTS = [260, 275] #[min, max]
+
+# Spend diamonds
+SPEND_DIAMONDS = True # True/False
+
+# Sleep between games
+SLEEP_GAME_TIME = [9,21] #[min,max]
+
+# mini sleep
+MINI_SLEEP = [3,7] #[min,max]
+
+# add sleep after 8 hours
+SLEEP_8HOURS = [60*60,120*60] #[min,max] seconds
+
+hello ='''
+ 
+▒█▀▀█ █░░ █░░█ █▀▄▀█ ▒█▀▀█ █▀▀█ ▀▀█▀▀ 
+▒█▀▀▄ █░░ █░░█ █░▀░█ ▒█▀▀▄ █░░█ ░░█░░ 
+▒█▄▄█ ▀▀▀ ░▀▀▀ ▀░░░▀ ▒█▄▄█ ▀▀▀▀ ░░▀░░
+                                          
+'''
